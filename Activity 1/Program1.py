31839fbd64af5671fb30a52d3186b9a2776927a7
DenJiwoo <149882571+DenJiwoo@users.noreply.github.com>
2024-08-21 18:36:10 +0800

    Create Program1.py

---
+++ Activity 1/Program1.py
@@ -0,0 +1,5 @@
+import random #Luis is so fucking gay !!!
+
+randomNumbers = [random.randint (1, 99) for _ in range(10)] #Generates random numbers with random. with the range of 10
+
+print(f"Array of random numbers are: {randomNumbers}")

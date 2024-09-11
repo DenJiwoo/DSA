e50bd4e6a28d155b8ef093905a37157f4eda22ab
DenJiwoo <149882571+DenJiwoo@users.noreply.github.com>
2024-09-11 22:45:58 +0800

    Update ui_components.py

--- LinkedList/routineWorkout_wGui/ui_components.py
+++ LinkedList/routineWorkout_wGui/ui_components.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from tkinter import PhotoImage
 from PIL import Image, ImageTk
-from PIL import Image, ImageTk, ImageDraw
 import tkinter.font as tkFont
 from workout_logic import WorkoutRoutine
 
@@ -24,76 +23,123 @@ class WorkoutPlanner:
         self.custom_font = tkFont.Font(family="Lato", size=12, weight="normal", slant="italic") if "Lato" in tkFont.families() else tkFont.Font(family="Arial", size=12, slant="italic")
         self.root.resizable(False, False)
         self.routine = WorkoutRoutine()
-        self.create_widgets()
+        self.create_widgets()      
 
     def create_widgets(self):
         self.root.config(background="#4D4D4D")
+    
+        # Create the day label
         self.day_label = tk.Label(self.root, text="Select Day: ", font=("Lato", 15, "bold italic"), 
                                   bg='#222222', foreground="white")
         self.day_label.pack(pady=10)
+    
+        # Create the day menu
         self.day_var = tk.StringVar(value="Sunday")
-
         self.day_menu = ttk.Combobox(self.root, textvariable=self.day_var, value=self.get_days(), state='readonly')
-        self.day_menu.pack(pady=10)
+        self.day_menu.pack(pady=20)
 
+        # Create navigation frame and buttons
         self.navigation_frame1 = tk.Frame(self.root, bg='#292929')
         self.navigation_frame1.pack(pady=10)
-
-        self.previous_day_button = tk.Button(self.navigation_frame1, text="Previous Day", command=self.show_previous_day_workouts, font=self.custom_font, relief="raised", bd=5)
+    
+        self.previous_day_button = tk.Button(self.navigation_frame1, text="Previous Day", 
+                                             command=self.show_previous_day_workouts, font=self.custom_font, 
+                                             relief="raised", bd=5, cursor="hand2")
         self.previous_day_button.grid(row=0, column=0, padx=5)
+        self.previous_day_button.bind("<Enter>", lambda e: self.on_hover(self.previous_day_button))
+        self.previous_day_button.bind("<Leave>", lambda e: self.off_hover(self.previous_day_button))
 
-        self.next_day_button = tk.Button(self.navigation_frame1, text="Next Day", command=self.show_next_day_workouts, font=self.custom_font, relief="raised", bd=5)
+        self.next_day_button = tk.Button(self.navigation_frame1, text="Next Day", 
+                                         command=self.show_next_day_workouts, font=self.custom_font, 
+                                         relief="raised", bd=5, cursor="hand2")
         self.next_day_button.grid(row=0, column=1, padx=5)
+        self.next_day_button.bind("<Enter>", lambda e: self.on_hover(self.next_day_button))
+        self.next_day_button.bind("<Leave>", lambda e: self.off_hover(self.next_day_button))
 
-        self.rest_day_button = tk.Button(self.root, text="Set as Rest Day", command=self.set_rest_day, font=self.custom_font, relief="raised", bd=5)
-        self.rest_day_button.pack(pady=10)
 
-        self.workout_day_button = tk.Button(self.root, text="Set as Workout Day", command=self.set_workout_day, font=self.custom_font, relief="raised", bd=5)
-        self.workout_day_button.pack(pady=10)
+        # Create navigation frame 2 for rest day and workout day buttons
+        self.navigation_frame2 = tk.Frame(self.root, bg='#303030')
+        self.navigation_frame2.pack(pady=15)
+        
+        self.rest_day_button = tk.Button(self.navigation_frame2, text="Set as Rest Day", 
+                                         command=self.set_rest_day, font=self.custom_font, 
+                                         relief="raised", bd=5, cursor="hand2")
+        self.rest_day_button.grid(row=0, column=0, padx=5)
+        self.rest_day_button.bind("<Enter>", lambda e: self.on_hover(self.rest_day_button))
+        self.rest_day_button.bind("<Leave>", lambda e: self.off_hover(self.rest_day_button))
 
+        self.workout_day_button = tk.Button(self.navigation_frame2, text="Set as Workout Day", 
+                                            command=self.set_workout_day, font=self.custom_font, 
+                                            relief="raised", bd=5, cursor="hand2")
+        self.workout_day_button.grid(row=0, column=1, padx=5)
+        self.workout_day_button.bind("<Enter>", lambda e: self.on_hover(self.workout_day_button))
+        self.workout_day_button.bind("<Leave>", lambda e: self.off_hover(self.workout_day_button))
+
+        # WORKOUT name entry box
         self.workout_label = tk.Label(self.root, text="Workout Name:", font=("Lato", 15, "bold italic"), 
-                                  bg='#343434', foreground="white")
+                                      bg='#343434', foreground="white")
         self.workout_label.pack(pady=5)
         self.workout_entry = tk.Entry(self.root, font=('Arial', 12, "bold"))
         self.workout_entry.pack(pady=5)
 
+        # Number of SETS entry box
         self.Sets_label = tk.Label(self.root, text="Sets:", font=("Lato", 15, "bold italic"), 
-                                  bg='#323232', foreground="white")
+                                   bg='#323232', foreground="white")
         self.Sets_label.pack(pady=5)
         self.Sets_entry = tk.Entry(self.root, font=('Arial', 12, "bold"))
         self.Sets_entry.pack(pady=5)
 
+        # Number of REPS entry box
         self.Reps_label = tk.Label(self.root, text="Reps:", font=("Lato", 15, "bold italic"), 
-                                  bg='#373737', foreground="white")
+                                   bg='#373737', foreground="white")
         self.Reps_label.pack(pady=5)
         self.Reps_entry = tk.Entry(self.root, font=('Arial', 12, "bold"))
         self.Reps_entry.pack(pady=5)
 
+        # SECONDS OF REST entry box
         self.Rest_label = tk.Label(self.root, text="Rest (Secs):", font=("Lato", 15, "bold italic"), 
-                                  bg='#404040', foreground="white")
+                                    bg='#404040', foreground="white")
         self.Rest_label.pack(pady=5)
         self.Rest_entry = tk.Entry(self.root, font=('Arial', 12, "bold"))
         self.Rest_entry.pack(pady=10)
-        
-        self.navigation_frame2 = tk.Frame(self.root, bg='#4D4D4D')
-        self.navigation_frame2.pack(pady=15)
+    
+    
+        # Create navigation frame 3 for add and delete buttons
+        self.navigation_frame3 = tk.Frame(self.root, bg='#4D4D4D')
+        self.navigation_frame3.pack(pady=15)
 
-        self.add_button = tk.Button(self.navigation_frame2, text="Add Workout", command=self.add_workout, font=self.custom_font, relief="raised", bd=5)
+        self.add_button = tk.Button(self.navigation_frame3, text="Add Workout", 
+                                    command=self.add_workout, font=self.custom_font, 
+                                    relief="raised", bd=5, cursor="hand2")
         self.add_button.grid(row=0, column=1, padx=5)
+        self.add_button.bind("<Enter>", lambda e: self.on_hover(self.add_button))
+        self.add_button.bind("<Leave>", lambda e: self.off_hover(self.add_button))
 
-        self.delete_button = tk.Button(self.navigation_frame2, text="Delete Workout", 
+        self.delete_button = tk.Button(self.navigation_frame3, text="Delete Workout", 
                                        command=self.delete_workout, font=self.custom_font, 
-                                       relief="raised", bd=5)
+                                       relief="raised", bd=5, cursor="hand2")
         self.delete_button.grid(row=0, column=0, padx=5)
+        self.delete_button.bind("<Enter>", lambda e: self.on_hover(self.delete_button))
+        self.delete_button.bind("<Leave>", lambda e: self.off_hover(self.delete_button))
 
+        # Displays the overall WEEKLY ROUTINE
         self.display_button = tk.Button(self.root, text="Display Weekly Routine", 
-                                        command=self.show_display_window, 
-                                        font=self.custom_font, relief="raised", bd=5)
+                                        command=self.show_display_window, font=self.custom_font, 
+                                        relief="raised", bd=5, cursor="hand2")
         self.display_button.pack(pady=10)
+        self.display_button.bind("<Enter>", lambda e: self.on_hover(self.display_button))
+        self.display_button.bind("<Leave>", lambda e: self.off_hover(self.display_button))
 
+        # CreateS the SINGLE workout display
         self.workout_display = tk.Text(self.root, height=10, width=50, 
-                                       state=tk.DISABLED, font=self.custom_font)
+                                       state=tk.DISABLED, font=('Arial', 12, "bold"))
         self.workout_display.pack(pady=10)
+
+    def on_hover(self, button):
+        button.config(bg="lightgray")  # Change background color on hover
+
+    def off_hover(self, button):
+        button.config(bg="SystemButtonFace")  # Reset background color when not hovering
 
     def get_days(self):
         days = []
@@ -158,10 +204,13 @@ class WorkoutPlanner:
         display_window.geometry("600x400")
         display_window.resizable(False, False)
 
-        frame = tk.Frame(display_window)
+        frame = tk.Frame(display_window, bg='#343434')
         frame.pack(fill=tk.BOTH, expand=True)
 
-        routine_text = tk.Text(frame, height=20, width=70, wrap=tk.WORD)
+        # Create the Text widget with the background color
+        routine_text = tk.Text(frame, height=20, width=70, 
+                               wrap=tk.WORD, bg='#343434', fg='white',
+                               font=("Lato", 15, "bold italic"))
         routine_text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
         scrollbar = tk.Scrollbar(frame, orient=tk.VERTICAL, command=routine_text.yview)
@@ -171,8 +220,7 @@ class WorkoutPlanner:
         routines = self.routine.display_routine()
         for day, routine in routines.items():
             routine_text.insert(tk.END, f"{day}:\n{routine}\n\n")
-         # Disable the Text widget to make it read-only
-        routine_text.config(state=tk.DISABLED)
+        routine_text.config(state=tk.DISABLED) # Disable the Text widget to make it read-only
 
     def show_next_day_workouts(self):
         next_day = self.routine.next_day()
